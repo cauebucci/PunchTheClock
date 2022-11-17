e8540bb90b5549dea9316912cbe68968721fa5a0
Cauê Lucas <36139948+cauebucci@users.noreply.github.com>
2022-11-17 14:23:44 -0300

    Finalizado filtro da tabela

--- routes/admin.ts
+++ routes/admin.ts
@@ -29,16 +29,16 @@ class AdminRoute {
         //     { id: 11, funcionario: "Jonas Pedro Figueiredo", entrada: "11/09/2022 09:40", iintervalo: "11/09/2022 12:31",  fintervalo: "13/10/2022 13:35", saida: "11/09/2022 19:34", tintervalo: "01:00:23", ttrabalho: "10:28:02"},
         // ];
 
-		await app.sql.connect(async (sql: app.Sql) => {
-			dados = await sql.query("SELECT HorarioID, F.FuncNome, HorarioEntrada, HorarioIIntervalo, HorarioVIntervalo, HorarioSaida from horarios H inner join funcionario F on H.FuncID = F.FuncID;");
-		});
+		//await app.sql.connect(async (sql: app.Sql) => {
+		//	dados = await sql.query("SELECT HorarioID, F.FuncNome, HorarioEntrada, HorarioIIntervalo, HorarioVIntervalo, HorarioSaida from horarios H inner join funcionario F on H.FuncID = F.FuncID;");
+		//});
 		await app.sql.connect(async (sql: app.Sql) => {
 			funcionarios = await sql.query("SELECT FuncNome from funcionario;");
 		});
 
 		res.render("admin/index", {
 			titulo: "Admin",
-			dados: dados,
+			//dados: dados,
 			funcionarios: funcionarios
 		});
 	}
@@ -86,6 +86,49 @@ class AdminRoute {
 
 		res.json(true);
 	}
+	
+	public async filtrar(req: app.Request, res: app.Response) {
+		let funcionario = parseInt(req.query["funcionario"] as string);
+		let de = req.query["de"];
+		let ate = req.query["ate"];
+
+		let where = "";
+		let parametros: any[] = [];
+
+
+		if (funcionario) {
+			if (where)
+				where += " AND ";
+			where += "F.FuncID = ?";
+			parametros.push(funcionario);
+		}
+
+		if (de) {
+			if (where)
+				where += " AND ";
+			where += "HorarioEntrada >= ?";
+			parametros.push(de);
+		}
+
+		if (ate) {
+			if (where)
+				where += " AND ";
+			where += "HorarioSaida <= ?";
+			parametros.push(ate);
+		}
+
+		let dados: any[];
+
+		if (where)
+			where = " WHERE " + where;
+
+		await app.sql.connect(async (sql: app.Sql) => {
+			dados = await sql.query("SELECT H.FuncID, F.FuncEmail,date_format(F.FuncNiver, '%d/%m/%Y') FuncNiver, T.TipoNome,HorarioID, F.FuncNome, date_format(HorarioEntrada, '%d/%m/%Y %H:%i') HorarioEntrada, date_format(HorarioIIntervalo, '%d/%m/%Y %H:%i') HorarioIIntervalo, date_format(HorarioVIntervalo, '%d/%m/%Y %H:%i') HorarioVIntervalo, date_format(HorarioSaida, '%d/%m/%Y %H:%i') HorarioSaida, TIMESTAMPDIFF(minute, HorarioIIntervalo, HorarioVIntervalo) minutos_intervalo, (TIMESTAMPDIFF(minute, HorarioEntrada, HorarioIIntervalo) + TIMESTAMPDIFF(minute, HorarioVIntervalo, HorarioSaida)) minutos_trabalhados from horarios H inner join funcionario F on H.FuncID = F.FuncID inner join tipo T on F.TipoID = T.TipoID" + where, parametros);
+		});
+
+		res.json(dados);
+	}
+
 }
 
 export = AdminRoute;
